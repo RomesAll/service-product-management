1a88aeb3f497a880ab4ff5073195d1f122b01161
RomesAll <romesky845@gmail.com>
2026-01-13 03:43:44 +0300

    review кода: 1) добавил фильтр для логгирования, чтобы ошибки, которые выше по иерархии не попадали во все файлы 2) добавил функцию в для логгирования запроса

--- app/core/logging_config.py
+++ app/core/logging_config.py
@@ -1,8 +1,18 @@
 from typing import Literal
+from app.core.config import BASE_DIR
 import logging
 
+class LevelFilter(logging.Filter):
+
+    def __init__(self, level):
+        self.level = level
+
+    def filter(self, record):
+        return record.levelno == self.level
+
 class LoggingSettings:
-    def __init__(self, logger_name: str = 'app', level: Literal["CRITICAL", "FATAL", "ERROR", "WARNING", "INFO", "DEBUG", "NOTSET"] = logging.INFO):
+
+    def __init__(self, logger_name: str, level: str | int):
         self.logger = logging.getLogger(logger_name)
         self.logger.setLevel(level)
 
@@ -10,26 +20,21 @@ class LoggingSettings:
     def create_formatter(format: str = "%(asctime)s - %(name)s - %(module)s - %(levelname)s - %(message)s"):
         return logging.Formatter(format)
 
-    def set_level(self, level: Literal["CRITICAL", "FATAL", "ERROR", "WARNING", "INFO", "DEBUG", "NOTSET"]):
-        self.logger.setLevel(level)
-
-    def get_logger(self):
-        return self.logger
-
-    def create_handler_console(self, level = logging.DEBUG, formatter: str = '%(asctime)s - %(name)s - %(module)s - %(levelname)s - %(message)s'):
+    def create_handler_console(self, level: str | int, formatter: str = '%(asctime)s - %(name)s - %(module)s - %(levelname)s - %(message)s'):
         handler = logging.StreamHandler()
         handler.setLevel(level)
         handler.setFormatter(self.create_formatter(formatter))
         self.logger.addHandler(handler)
 
-    def create_handler_file(self, filename: str, filemode: str, level = logging.DEBUG, formatter: str = '%(asctime)s - %(name)s - %(module)s - %(levelname)s - %(message)s'):
-        handler = logging.FileHandler(filename, mode=filemode)
+    def create_handler_file(self, filename: str, filemode: str, level: str | int, formatter: str = '%(asctime)s - %(name)s - %(module)s - %(levelname)s - %(message)s'):
+        handler = logging.FileHandler(filename, filemode, delay=True)
         handler.setLevel(level)
         handler.setFormatter(self.create_formatter(formatter))
+        handler.addFilter(LevelFilter(level))
         self.logger.addHandler(handler)
 
 logging_setup = LoggingSettings(logger_name="app", level="DEBUG")
 logging_setup.create_handler_console(level=logging.DEBUG)
-logging_setup.create_handler_file(filename='info.log', filemode='a', level=logging.INFO)
-logging_setup.create_handler_file(filename='warning.log', filemode='a', level=logging.WARNING)
-logging_setup.create_handler_file(filename='error.log', filemode='a', level=logging.ERROR)
+logging_setup.create_handler_file(filename=f'{BASE_DIR}/app/logs/info.log', filemode='a', level=logging.INFO)
+logging_setup.create_handler_file(filename=f'{BASE_DIR}/app/logs/warning.log', filemode='a', level=logging.WARNING)
+logging_setup.create_handler_file(filename=f'{BASE_DIR}/app/logs/error.log', filemode='a', level=logging.ERROR)
--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request, Depends, Response, status
 from core import settings
 from app.api.v1 import procurements_router, type_products_router, products_router, auth_router, users_router
 from app.core.exception_handlers import exception_handler
 from datetime import datetime, timezone
 from app.core.logging_config import *
-import uvicorn, time
+import uvicorn, time, logging
 
 app = FastAPI()
 exception_handler(app)
@@ -14,6 +14,18 @@ app.include_router(procurements_router)
 app.include_router(auth_router)
 app.include_router(users_router)
 
+def logging_request(request: Request, response: Response, time_processing):
+    message = ("client: {0} url: {1} method: {2} status: {3} time: {4}"
+               .format(request.client.host, request.url, request.method, response.status_code, time_processing))
+    if request.url.path in ["/health-check", '/docs', '/favicon.ico', '/openapi.json']:
+        settings.logger.debug(message)
+    elif 100 <= response.status_code < 400:
+        settings.logger.info(message)
+    elif 400 <= response.status_code < 500:
+        settings.logger.warning(message)
+    elif 500 <= response.status_code < 600:
+        settings.logger.error(message)
+
 @app.middleware("http")
 async def process_request(request: Request, call_next):
     time_start = time.time()
@@ -21,12 +33,7 @@ async def process_request(request: Request, call_next):
     end_time = time.time() - time_start
     response.headers["X-Processing-Time-ms"] = str(end_time.__round__(4))
     response.headers['X-Date-Request'] = str(datetime.now(timezone.utc))
-    if request.url.path in ["/health-check", '/docs', '/favicon.ico', '/openapi.json']:
-        settings.logger.debug("client: %s url: %s method: %s", request.client.host, request.url, request.method)
-    else:
-        settings.logger.info("client: %s url: %s method: %s", request.client.host, request.url, request.method)
-        settings.logger.info("client: %s url: %s method: %s status: %s time: %s", request.client.host, request.url,
-                             request.method, response.status_code, str(end_time))
+    logging_request(request, response, end_time)
     return response
 
 @app.get("/health-check")
