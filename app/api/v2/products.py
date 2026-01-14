bdd5f7cd4d24a07c16b0f4fb0ea66314789a3a43
RomesAll <romesky845@gmail.com>
2026-01-14 09:29:10 +0300

    убрал лишние ручки у api/v2

--- app/api/v2/products.py
+++ app/api/v2/products.py
@@ -17,18 +17,3 @@ def get_all_records(request: Request, session: session_dep, pagination: paginati
 def get_records_by_id(request: Request, id: int, session: session_dep):
     result = ProductsService(session, request.client.host).get_records_by_id(id)
     return {'message': result}
-
-@router.post('/')
-def create_records(request: Request, dto_model: ProductsPOSTSchemas, session: session_dep):
-    result = ProductsService(session, request.client.host).create_records(dto_model)
-    return {'message': result}
-
-@router.put('/')
-def update_records(request: Request, dto_model: ProductsPUTSchemas, session: session_dep):
-    result = ProductsService(session, request.client.host).update_records(dto_model)
-    return {'message': result}
-
-@router.delete('/{id}')
-def delete_records(request: Request, id: int, session: session_dep):
-    result = ProductsService(session, request.client.host).delete_records(id)
-    return {'message': result}
